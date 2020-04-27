69f0e75cb087493bf123164febf32e6069d3325a
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-27 20:50:14 +0200

    main:

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ extern crate dirs;
 use std::env;
 use std::path::Path;
 use std::ffi::OsString;
+use std::path::PathBuf;
 
 fn main() {
-    //let path = dirs::config_dir();
-
     let os = std::env::consts::OS;
 
     if os == "linux" {
-        let path = dirs::home_dir();
-        env::set_current_dir(path);
-        env::set_current_dir("/.mozilla/firefox");
+        let mut completePath = PathBuf::new();
+        let temp: PathBuf = dirs::home_dir().unwrap();
+        completePath.push(temp);
+        // completePath.push("/.mozilla/firefox");
+        println!("{:?}", completePath.to_str());
+        //env::set_current_dir(path);
+        //env::set_current_dir("/.mozilla/firefox");
     }
 }
