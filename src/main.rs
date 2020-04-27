6babeabd357259bdea239b44d8cee2f563d1254e
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-27 18:50:14 +0200

    main:

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-//use std::process::Command;
-//use std::env;
-//use std::path::Path;
 extern crate dirs;
+use std::env;
+use std::path::Path;
+use std::ffi::OsString;
 
 fn main() {
-    let path = dirs::config_dir();
+    //let path = dirs::config_dir();
 
-    
+    let os = std::env::consts::OS;
+
+    if os == "linux" {
+        let path = dirs::home_dir()
+        env::set_current_dir(let + "/.mozilla/firefox")
+    }
 }
