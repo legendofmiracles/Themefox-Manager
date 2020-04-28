f40561c5e4219a840e1eb446c901c0dc7adcd8b1
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-28 06:13:27 +0200

    main:

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::env;
 use std::path::Path;
 use std::ffi::OsString;
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let os = std::env::consts::OS;
@@ -12,7 +14,15 @@ fn main() {
         let temp: PathBuf = dirs::home_dir().unwrap();
         completePath.push(temp);
         completePath.push(".mozilla/firefox");
-        println!("{:?}", completePath.to_str());
         env::set_current_dir(completePath);
+        let file = openFile();
+        println!("{:?}", file);
+        
     }
+}
+
+
+fn openFile() -> std::io::Result<()> {
+    let mut f = File::open("installs.ini")?;
+    Ok(())
 }
