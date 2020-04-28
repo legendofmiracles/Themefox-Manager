8f6b603c8bbf3f3c89599511d44370f009b9cc7e
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-28 06:35:30 +0200

    main:

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 extern crate dirs;
 use std::env;
-use std::path::Path;
-use std::ffi::OsString;
+//use std::path::Path;
+//use std::ffi::OsString;
 use std::path::PathBuf;
 use std::fs::File;
-use std::io::prelude::*;
+//use std::io::prelude::*;
 
-fn main() {
+fn main() -> std::io::Result<()>{
     let os = std::env::consts::OS;
 
     if os == "linux" {
         let mut completePath = PathBuf::new();
+        
         let temp: PathBuf = dirs::home_dir().unwrap();
+        
         completePath.push(temp);
+        
         completePath.push(".mozilla/firefox");
+        
         env::set_current_dir(completePath);
+        
         let mut file = File::open("installs.ini");
+        
         let mut contents = String::new();
+        
         file.read_to_string(&mut contents);
+        
         println!("{:?}", file);
         
     }
