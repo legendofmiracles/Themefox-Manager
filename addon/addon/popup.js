595f20d33783884063601061ee902c67e83c3265
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-06-05 09:46:05 +0200

    the addon now has a good textbox

--- addon/addon/popup.js
+++ addon/addon/popup.js
@@ -49,9 +49,12 @@ function install() {
     var injection = document.createElement("textarea")
     document.getElementById("install-button").after(injection, { id: "textarea"});
     let textarea = document.getElementsByTagName("textarea");
+    textarea[0].before(document.createElement("br"));
+    textarea[0].before(document.createElement("br"));
     textarea[0].toggleAttribute("readonly");
     textarea[0].id = "textarea"
     textarea[0].rows = 20;
+    textarea[0].cols= 100;
     textarea[0].placeholder = "The output from the native application will appear here."
     textarea[0].value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut facilisis, arcu vitae adipiscing placerat, nisl lectus accumsan nisi, vitae iaculis sem neque vel lectus. Praesent tristique commodo lorem quis fringilla. Sed ac tellus eros. Sed consectetur eleifend felis vitae luctus. Praesent sagittis, est eget bibendum tincidunt, ligula diam tincidunt augue, a fermentum odio velit eget mi. Phasellus mattis, elit id fringilla semper, orci magna cursus ligula, non venenatis lacus augue sit amet dui. Pellentesque lacinia odio id nisi pulvinar commodo tempus at odio. Ut consectetur eros porttitor nunc mollis ultrices. Aenean porttitor, purus sollicitudin viverra auctor, neque erat blandit sapien, sit amet tincidunt massa mi ac nibh. Proin nibh sem, bibendum ut placerat nec, cursus et lacus. Phasellus vel augue turpis. Nunc eu mauris eu leo blandit mollis interdum eget lorem. ";
   } else {
